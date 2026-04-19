e379a1f5ee8538de785433b94281874fa799c078
joicedenicia <joicedenicia.27csa@licet.ac.in>
2026-04-19 21:12:59 +0530

    first commit

---
+++ src/main/java/com/example/azure_maven/Ex9devopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex9devopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex9devopsApplication.class, args);
+	}
+
+}
